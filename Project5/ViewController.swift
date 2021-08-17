573f2c364edc8f973ed66b84835d70d89ab261db
Yuryi Ustsinovich <ustinhtc@gmail.com>
2021-08-17 21:21:12 +0300

    add pop up for submitting users words

--- Project5/ViewController.swift
+++ Project5/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        
         if let startWordsUrl = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordsUrl) {
                 allWords = startWords.components(separatedBy: "\n")
@@ -25,6 +27,25 @@ class ViewController: UITableViewController {
         }
         
         startGame()
+    }
+    
+    @objc func promptForAnswer() {
+        let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) {
+            //code of closure callback here
+            [weak self, weak ac] _ in
+            guard let answer = ac?.textFields?[0].text else { return }
+            self?.submit(answer)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func submit(_ answer: String) {
+        
     }
 
     func startGame() {
